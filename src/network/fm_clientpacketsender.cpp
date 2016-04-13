15fae3109a7ebb1b33f6f655ad59dde259723304
orlovv <orlovv@herzen.spb.ru>
2016-04-13 20:40:38 +0300

    previous commit refactoring

--- src/network/fm_clientpacketsender.cpp
+++ src/network/fm_clientpacketsender.cpp
@@ -315,4 +315,11 @@ void Client::sendDrawControl() {
 	Send(0, buffer, false);
 }
 
-#include "../km/network/km_clientpacketsender.cpp"
+void Client::sendChatOpened(bool opened)
+{
+    MSGPACK_PACKET_INIT(TOSERVER_IS_CHAT_OPENED, 1);
+	PACK(TOSERVER_IS_OPENED, opened);
+
+	Send(0, buffer, true);
+    return;
+}
